5f83af4443d4d48be05cc8a60e02f740510b5d61
loading-error303 <Karna.suresh@outlook.com>
2026-09-07 11:12:18 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "base_template_1.21.11"
---
+++ src/main/java/Enders_Armor/base_template_12111/Base_template_12111.java
@@ -0,0 +1,16 @@
+package Enders_Armor.base_template_12111;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Base_template_12111 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
